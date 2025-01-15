cc5e68bdb63f78c5d9b7085a122026ec7084d61b
Nadeesha Sumanasekara <118651477+NadeeshaSumanasekara@users.noreply.github.com>
2025-01-15 13:24:49 +0530

    added file

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  output: "export",
+};
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: "export", // Use static site generation
+//   basePath: "/my-portfolio", // Replace <repository-name> with your GitHub repo name
+//   assetPrefix: "/my-portfolio", // Replace <repository-name> with your GitHub repo name
+// };
+
+// module.exports = nextConfig;
