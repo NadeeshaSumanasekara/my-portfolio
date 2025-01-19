ec499d9a506e53a5026211c957ece44845135426
Nadeesha Sumanasekara <118651477+NadeeshaSumanasekara@users.noreply.github.com>
2025-01-19 19:01:38 +0530

    config file edited

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+const repoName = "my-portfolio";
+
 const nextConfig = {
   output: "export", // Use static site generation
-  basePath: "/my-portfolio",
+  assetPrefix: isProd ? `./` : "",
+  basePath: isProd ? `/${repoName}` : "",
+  trailingSlash: true, // Ensures proper static site behavior
+  images: {
+    unoptimized: true, // Required for static exports with GitHub Pages
+  },
 };
 
 module.exports = nextConfig;
